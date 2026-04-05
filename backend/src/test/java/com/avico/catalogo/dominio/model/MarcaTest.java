196c8eff59eaf25220bc1093f5d801b8ebc0e02c
Tsutsumi <tsutsumi@comunidad.unam.mx>
2026-04-05 16:22:21 -0600

    Objeto de valor marca con pruebas unitarias

---
+++ backend/src/test/java/com/avico/catalogo/dominio/model/MarcaTest.java
@@ -0,0 +1,50 @@
+package com.avico.catalogo.dominio.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MarcaTest {
+
+    @Test
+    void crearMarca() {
+
+        //Given
+        String marca = "Marca";
+
+        //When
+        Marca marcaObj = new Marca(marca);
+
+        //Then
+        assertEquals("Marca", marcaObj.marca());
+    }
+
+    @Test
+    void crearMarcaDeberiaLanzarExcepcionCuandoLaMarcaEsNula() {
+
+        //Given
+        String marca = null;
+
+        //When
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> new Marca(marca));
+
+        //Then
+        assertEquals("La marca es obligatoria.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   "})
+    void crearMarcaDeberiaLanzarExcepcionCuandoLaMarcaEstaVacia(String marca) {
+        //Given
+
+        //When
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Marca(marca));
+
+        //When
+        assertEquals("La marca no puede estar vacía.", exception.getMessage());
+
+    }
+}
